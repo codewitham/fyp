f7990014c775238150c7a9f18ffa92443728a505
SniperGod <103439202+AkshatMahadeva@users.noreply.github.com>
2024-04-08 07:36:24 +0530

    dash height

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const page = () => {
     return (
-        <div className='h-screen bg-gray-100'>
+        <div className='min-h-screen bg-gray-100'>
             <BgNavbar />
             <DashboardPage />
         </div>
